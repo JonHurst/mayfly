4f10107169cf983e159597cc635d360247a50235
Jon Hurst <jon@hursts.org.uk>
2020-01-25 11:32:58 +0000

    Major refactor

--- mayfly.py
+++ mayfly.py
@@ -3,8 +3,9 @@
 import sys
 import os
 import csv
-from typing import NamedTuple, List, Dict, Tuple
+from typing import NamedTuple, List, Dict, Tuple, Optional
 import datetime
+import getpass
 
 import templates
 import flight_info
@@ -12,11 +13,12 @@ import flight_info
 ezy_operator_ids = ["EZY", "EJU", "EZS"]
 
 class Service(NamedTuple):
+    type_: str
     dt: datetime.datetime
     operator_id: str
     service_id: str
     dest_or_orig: str
-    delay: int = 0
+    delay: Optional[int] = None
 
 
 class MayflyBin(NamedTuple):
@@ -24,27 +26,83 @@ class MayflyBin(NamedTuple):
     departures: List[Service]
 
 
-def process_csv(data: List[str], updates: Dict[Service, Service]
-) -> Dict[datetime.datetime, MayflyBin]:
+def process_csv(data: List[str]) -> List[Service]:
+    """Map a list of lines of CSV data into a list of Service tuples
+
+    Args:
+       data: List of strings representing lines of a csv file
+"""
     reader = csv.reader(data)
-    retval: Dict[datetime.datetime, MayflyBin] = {}
+    retval: List[Service] = []
     for row in reader:
         #todo: check data integrity
         dt_string = row[0] + row[10]
         dt = datetime.datetime.strptime(dt_string, "%d/%m/%Y%H%M")
         #todo: convert time to UTC
-        service = Service(dt, row[2], row[3], row[4])
-        if service in updates:
-            service = updates[service]
+        retval.append(Service(
+            type_=row[1],
+            dt=dt,
+            operator_id=row[2],
+            service_id=row[3],
+            dest_or_orig=row[4]))
+    return retval
+
+
+def update_services_from_AIMS(services: List[Service]
+) -> Optional[List[Service]]:
+    try:
+        flights = flight_info.get_AIMS_flights(
+            os.getenv("AIMSPASSWORD") or getpass.getpass(),
+            datetime.date.today(), 2)
+        updates = {}
+        for f in flights:
+            if f.from_ == "BRS":
+                type_ = "D"
+                delay = f.off - f.sched_off
+                dest_or_orig = f.to
+                dt = f.sched_off
+                new_dt = f.off
+            else:
+                type_ = "A"
+                delay = f.on - f.sched_on
+                dest_or_orig = f.from_
+                dt = f.sched_on
+                new_dt = f.on
+            updates[Service(type_=type_, dt=dt,
+                            operator_id=f.operator,
+                            service_id=f.flight_num,
+                            dest_or_orig=dest_or_orig)] = (
+                    Service(type_=type_, dt=new_dt,
+                            operator_id=f.operator,
+                            service_id=f.flight_num,
+                            dest_or_orig=dest_or_orig,
+                            delay=int(delay.total_seconds() / 60)))
+        retval: List[Service] = []
+        for s in services:
+            if s in updates:
+                retval.append(updates[s])
+            else:
+                retval.append(s)
+        return retval
+    except Exception as err:
+        #much can go wrong talking to AIMS, so just return None if it does.
+        print(err, file=sys.stderr)
+        return None
+
+
+def split_into_bins(services: List[Service]
+) -> Dict[datetime.datetime, MayflyBin]:
+    retval: Dict[datetime.datetime, MayflyBin] = {}
+    for service in services:
         if service.dt.minute < 30:
             bin_id = service.dt.replace(minute=0)
         else:
             bin_id = service.dt.replace(minute=30)
         if bin_id not in retval:
             retval[bin_id] = MayflyBin([], [])
-        if row[1] == "A":
+        if service.type_ == "A":
             retval[bin_id].arrivals.append(service)
-        elif row[1] == "D":
+        elif service.type_ == "D":
             retval[bin_id].departures.append(service)
     return retval
 
@@ -59,12 +117,17 @@ def build_service_list(services: List[Service]
     output_strings: List[str] = []
     for s in sorted(services, key=lambda x: x.dt):
         template = templates.nonezy_service_template
+        if s.delay is None:
+            late_str = ""
+            delay = 0
+        else:
+            late_str = "late" if s.delay > 0 else "not_late"
+            delay = s.delay
         if s.operator_id in ezy_operator_ids:
             output_strings.append(templates.ezy_service_template.format(
                 s.dt.strftime("%H:%M"),
                 "{}{} {}".format(s.operator_id, s.service_id, s.dest_or_orig),
-                "late" if s.delay > 0 else "not_late",
-                s.delay))
+                late_str, delay))
         else:
             output_strings.append(templates.nonezy_service_template.format(
                 s.dt.strftime("%H:%M"),
@@ -157,54 +220,11 @@ def build_page(data: Dict[datetime.datetime, MayflyBin],
             "".join(bin_list))))
 
 
-def create_update_dict():
-    flight_info.connect_via_ecrew("009448",
-                                  os.getenv("AIMSPASSWORD"))
-    d = datetime.date.today()
-    arrivals, departures = flight_info.get_flight_info(d)
-    ret = {}
-    for f in arrivals:
-        if f.sched_on == f.on_: continue
-        flight_num_components = f.flight_num.split()
-        service_id = flight_num_components[-1]
-        op_id = "EZY"
-        if len(flight_num_components) == 2:
-            op_id = flight_num_components[0]
-        delay = int((f.on_ - f.sched_on).total_seconds() / 60)
-        ret[Service(
-            dt=f.sched_on,
-            operator_id=op_id,
-            service_id=service_id,
-            dest_or_orig=f.from_)] = Service(dt=f.on_,
-                                             operator_id=op_id,
-                                             service_id=service_id,
-                                             dest_or_orig=f.from_,
-                                             delay=delay)
-    for f in departures:
-        if f.sched_off == f.off_: continue
-        flight_num_components = f.flight_num.split()
-        service_id = flight_num_components[-1]
-        op_id = "EZY"
-        if len(flight_num_components) == 2:
-            op_id = flight_num_components[0]
-        delay = int((f.off_ - f.sched_off).total_seconds() / 60)
-        ret[Service(
-            dt=f.sched_off,
-            operator_id=op_id,
-            service_id=service_id,
-            dest_or_orig=f.to)] = Service(dt=f.off_,
-                                          operator_id=op_id,
-                                          service_id=service_id,
-                                          dest_or_orig=f.to,
-                                          delay=delay)
-    flight_info.logout(True)
-    return ret
-
-
 def main(csv_filename: str, html_filename: str) -> None:
-    updates = create_update_dict()
     with open(csv_filename) as f:
-        bins = process_csv(f.readlines(), updates)
+        services = process_csv(f.readlines())
+        services = update_services_from_AIMS(services) or services
+        bins = split_into_bins(services)
         with open(html_filename, "w") as o:
             o.write(build_page(bins))
 
