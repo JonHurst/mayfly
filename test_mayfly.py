a7a77f3cc690d1b8b8c3388a670ca705a29d3d7c
Jon Hurst <jon@hursts.org.uk>
2020-01-29 13:17:03 +0000

    Added unittest for update_services_from_AIMS

--- test_mayfly.py
+++ test_mayfly.py
@@ -2,8 +2,38 @@ import unittest
 import mayfly
 import datetime
 import flight_info
+import os
+import getpass
 
 class TestMayfly(unittest.TestCase):
+
+    def setUp(self):
+        def monkey_patch_get_AIMS_flights(_1, _2, _3):
+            return [
+            flight_info.Flight(operator='EZY', flight_num='570', from_='BRS', to='NCL',
+                               type_='319', reg='G-EZBV',
+                               sched_off=datetime.datetime(2020, 1, 30, 20, 50),
+                               sched_on=datetime.datetime(2020, 1, 30, 21, 55),
+                               off=datetime.datetime(2020, 1, 30, 21, 50),
+                               on=datetime.datetime(2020, 1, 30, 22, 55)),
+            flight_info.Flight(operator='EZY', flight_num='571', from_='NCL', to='BRS',
+                               type_='319', reg='G-EZBV',
+                               sched_off=datetime.datetime(2020, 1, 31, 20, 50),
+                               sched_on=datetime.datetime(2020, 1, 31, 21, 55),
+                               off=datetime.datetime(2020, 1, 31, 20, 50),
+                               on=datetime.datetime(2020, 1, 31, 21, 55)),
+            ]
+        self.get_AIMS_flights_orig = flight_info.get_AIMS_flights
+        flight_info.get_AIMS_flights = monkey_patch_get_AIMS_flights
+        self.getpass_orig = getpass.getpass
+        def monkey_patch_getpass(): return None
+        getpass.getpass = monkey_patch_getpass
+
+
+    def tearDown(self):
+        flight_info.get_AIMS_flights = self.get_AIMS_flights_orig
+        getpass.getpass = self.getpass_orig
+
 
     def test_csv_import(self):
         data = ["06/01/2020,A,TOM,6751,TFS,GCTS,TFS,GCTS,"
@@ -72,3 +102,30 @@ class TestMayfly(unittest.TestCase):
         self.assertEqual(mayfly._make_update_dict([]), {})
         with self.assertRaises(AttributeError) as cm:
             mayfly._make_update_dict("wrong type of data")
+
+
+    def test_update_services_from_AIMS(self):
+        data = [
+            mayfly.Service(type_='D', dt=datetime.datetime(2020, 1, 30, 20, 50),
+                           operator_id='EZY', service_id='570',
+                           dest_or_orig='NCL', delay=None),
+            mayfly.Service(type_='A', dt=datetime.datetime(2020, 1, 31, 21, 55),
+                           operator_id='EZY', service_id='571',
+                           dest_or_orig='NCL', delay=None),
+        ]
+        result = [
+            mayfly.Service(type_='D', dt=datetime.datetime(2020, 1, 30, 21, 50),
+                           operator_id='EZY', service_id='570',
+                           dest_or_orig='NCL', delay=60),
+            mayfly.Service(type_='A', dt=datetime.datetime(2020, 1, 31, 21, 55),
+                           operator_id='EZY', service_id='571',
+                           dest_or_orig='NCL', delay=0),
+        ]
+        self.assertEqual(mayfly.update_services_from_AIMS(data), result)
+        self.assertEqual(mayfly.update_services_from_AIMS([]), [])
+        def raise_exception(_1, _2, _3):
+            raise ValueError("Test exception")
+        old = flight_info.get_AIMS_flights
+        flight_info.get_AIMS_flights = raise_exception
+        self.assertEqual(mayfly.update_services_from_AIMS(data), None)
+        flight_info.get_AIMS_flights = old
